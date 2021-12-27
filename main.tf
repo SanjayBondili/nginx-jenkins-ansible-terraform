e636937a4ee42e8807c06ee38d72c6689ee7d5e8
sanjay <sanjay@orcalabs.tech>
2021-12-27 11:16:04 +0530

    subnet web1 added

--- main.tf
+++ main.tf
@@ -14,11 +14,35 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-00f7e5c52c0f43726"
-  instance_type = "t2.micro"
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-00f7e5c52c0f43726"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     Name = "ExampleAppServerInstance"
+#   }
+# }
+resource "aws_vpc" "olvpc" {
+  cidr_block       = "192.168.0.0/16"
+  instance_tenancy = "default"
 
   tags = {
-    Name = "ExampleAppServerInstance"
+    Name = "olvpc"
+  }
+}
+resource "aws_subnet" "web1" {
+  vpc_id     = aws_vpc.olvpc.id
+  cidr_block = "192.168.0.0/24"
+
+  tags = {
+    Name = "web1"
+  }
+}
+resource "aws_subnet" "web2" {
+  vpc_id     = aws_vpc.olvpc.id
+  cidr_block = "192.168.1.0/24"
+
+  tags = {
+    Name = "web2"
   }
 }
