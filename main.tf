4d6c57ee72188d0d3aba436f0872767c95eea313
sanjay <sanjay@orcalabs.tech>
2021-12-28 14:07:52 +0530

    files segregated

--- main.tf
+++ main.tf
@@ -1,36 +1,19 @@
+terraform {
+    required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
 
-
-
-
-# resource "aws_instance" "app_server" {
-#   ami           = "ami-00f7e5c52c0f43726"
-#   instance_type = "t2.micro"
-
-#   tags = {
-#     Name = "ExampleAppServerInstance"
-#   }
-# }
+  required_version = ">= 0.14.9"
+}
 resource "aws_vpc" "olvpc" {
-  cidr_block       = "192.168.0.0/16"
+  cidr_block       = var.vpccidr
   instance_tenancy = "default"
 
   tags = {
     Name = "olvpc"
   }
 }
-resource "aws_subnet" "web1" {
-  vpc_id     = aws_vpc.olvpc.id
-  cidr_block = "192.168.0.0/24"
 
-  tags = {
-    Name = "web1"
-  }
-}
-resource "aws_subnet" "web2" {
-  vpc_id     = aws_vpc.olvpc.id
-  cidr_block = "192.168.1.0/24"
-
-  tags = {
-    Name = "web2"
-  }
-}
--- variables.tf
+++ variables.tf
@@ -1,10 +1,11 @@
 variable "region" {
   type = string
-  default = "us-west-2"
+  default = "us-west-1"
   description = "region in which olvpc has to be created"
 
 }
-variable "vpc_cidr" {
-    cidr_block= "192.168.0.0/16"
-  
+variable "vpccidr" {
+    type = string
+    default= "192.168.0.0/16"
+    description = "default cidr block of olvpc"
 }
